39a7ba7b9dbb9e2bfb2b4a34e2ce7ab055f638c8
haole <haole@qq.com>
2023-05-05 18:19:25 +0800

    wjz master first commit

---
+++ src/main/java/com/haole/ssm/App.java
@@ -0,0 +1,8 @@
+package com.haole.ssm;
+
+public class App {
+    public static void main(String[] args){
+        System.out.println("Hello git");
+        System.out.println("Hello git  200");
+    }
+}
