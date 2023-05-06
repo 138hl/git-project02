837adc477cc62632d43832d49fd30136b9c8f9f4
haole <haole@qq.com>
2023-05-06 08:28:16 +0800

    wjz master commit

--- src/main/java/com/haole/ssm/App.java
+++ src/main/java/com/haole/ssm/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args){
         System.out.println("Hello git");
         System.out.println("Hello git  200");
+        System.out.println("Hello git  300");
     }
 }
